58dd5c7212455334a6cafb2eaf773e15cc8fb86f
Witold Baran <baranvtek@gmail.com>
2011-11-16 10:43:58 +0100

    Klasa optymalizera, prace jeszcze trwaja

--- src/heartdoctor/ann/NeuralNetworkOptymalizator.java
+++ src/heartdoctor/ann/NeuralNetworkOptymalizator.java
@@ -13,72 +13,90 @@ import java.util.ArrayList;
  */
 public class NeuralNetworkOptymalizator implements NeuralNetworkTrainingListener{
 
+    static private int INPUTS = 14,OUTPUTS = 1;
+
     private NeuralNetworkTrainer _nnTrainer;
     private NeuralNetwork _network;
 
-    private int _minNumInputs = 1;
-    private int _minNumOutputs = 1;
     private int _minNumHiddenLayers = 1;
     private int _minNumNeuronsPerHiddenLayer = 1;
-  
-    private int _maxNumInputs;
-    private int _maxNumOutputs;
+    private double[] _learningRateSet;
+    private double[] _learningRateAdjustSet;
+    private double[] _momentumSet;
+    private double[] _forgettingRateSet;
+ 
     private int _maxNumHiddenLayers;
     private int _maxNumNeuronsPerHiddenLayer;
 
-    public int optimumInputs;
-    public int optimumOutputs;
     public int optimumHiddenLayers;
     public int optimumNeuronsPerHiddenLayer;
+    public double optimumLearningRate;
+    public double optimumLearningRateAdjust;
+    public double optimumMomentum;
+    public double optimumForgettingRate;
 
     DataSet trainingSet;
     DataSet generalizationSet;
     DataSet validationSet;
 
-    public NeuralNetworkOptymalizator(int nI,int nO, int nHL,int nNPH,DataSet data){
-        _maxNumInputs = nI;
-        _maxNumOutputs = nO;
+    public NeuralNetworkOptymalizator(int nHL,int nNPH,double[] lR,double[] lRA,double[] m,double[] fR,DataSet data){
         _maxNumHiddenLayers = nHL;
         _maxNumNeuronsPerHiddenLayer = nNPH;
+
         
         int i=data.entries.size();
+
+        // Wprowadzić loswo wybierane rekordy
         trainingSet.entries = (ArrayList<DataEntry>) data.entries.subList(0, (int)0.6*i);
         generalizationSet.entries = (ArrayList<DataEntry>) data.entries.subList((int)0.6*i+1, (int)0.8*i);
         validationSet.entries = (ArrayList<DataEntry>) data.entries.subList((int)0.8*i+1, i-1);
     }
     
-    public NeuralNetworkOptymalizator(int minI, int maxI,int minO, int maxO, int minHL,int maxHL,int minNPH,int maxNPH,DataSet data){
-        _minNumInputs = minI;
-        _minNumOutputs = minO;
+    public NeuralNetworkOptymalizator(int minHL,int maxHL,int minNPH,int maxNPH,double[] LR,double[] LRA,double[] M,double[] FR,DataSet data){
+
         _minNumHiddenLayers = minHL;
         _minNumNeuronsPerHiddenLayer = minNPH;
+        _learningRateSet = LR;
+        _learningRateAdjustSet = LRA;
+        _momentumSet = M;
+        _forgettingRateSet = FR;
         
-        _maxNumInputs = maxI;
-        _maxNumOutputs = maxO;
         _maxNumHiddenLayers = maxHL;
         _maxNumNeuronsPerHiddenLayer = maxNPH;
 
         int i=data.entries.size();
+        // Wprowadzić loswo wybierane rekordy
         trainingSet.entries = (ArrayList<DataEntry>) data.entries.subList(0, (int)0.6*i);
         generalizationSet.entries = (ArrayList<DataEntry>) data.entries.subList((int)0.6*i+1, (int)0.8*i);
         validationSet.entries = (ArrayList<DataEntry>) data.entries.subList((int)0.8*i+1, i-1);
     }
 
     public void optimalize(){
-        for(int i = _minNumInputs;i<=_maxNumInputs;i++)
-            for(int o = _minNumOutputs;o<=_maxNumOutputs; o++)
-                for(int h = _minNumHiddenLayers;h<=_maxNumHiddenLayers; h++)
-                    for(int p = _minNumNeuronsPerHiddenLayer;p<=_maxNumNeuronsPerHiddenLayer; p++){
+        for(int h = _minNumHiddenLayers; h <= _maxNumHiddenLayers; h++)
+            for(int p = _minNumNeuronsPerHiddenLayer; p <= _maxNumNeuronsPerHiddenLayer; p++)
+                for(int lr = 0 ; lr < _learningRateSet.length; lr++)
+                    for(int lra = 0; lra <= _learningRateAdjustSet.length; lra++)
+                        for(int m = 0; m <= _momentumSet.length; m++)
+                            for(int fr = 0; fr <= _forgettingRateSet.length ; fr++){
                         // DO SOMETHING AMAZING!!! -----------------------------
-                        _network = new NeuralNetwork(_minNumInputs, _minNumOutputs, _minNumHiddenLayers, _minNumNeuronsPerHiddenLayer);
-                        _nnTrainer = new NeuralNetworkTrainer(_network);
-                        _nnTrainer.setListener(this);
-                        _nnTrainer.trainNetwork(trainingSet, generalizationSet, validationSet);
-                    }
+                                _network = new NeuralNetwork(INPUTS, OUTPUTS, h, p);
+                                _nnTrainer = new NeuralNetworkTrainer(_network);
+                                _nnTrainer.setLearningRate(lr);
+                                _nnTrainer.setLearningRateAdjust(lra);
+                                _nnTrainer.setMomentumConst(m);
+                                _nnTrainer.setForgettingRate(fr);
+                                _nnTrainer.setListener(this);
+                                _nnTrainer.trainNetwork(trainingSet, generalizationSet, validationSet);
+                            }
+    }
+
+    public void setGui(){
+
     }
 
     public void updateTrainingSetAccuracy(double accuracy) {
         throw new UnsupportedOperationException("Not supported yet.");
+        // RYSYJEMY ZAJEBISTE WYKRESYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY!!!!!!!!!!!!
     }
 
     public void updateTrainingSetMSE(double mse) {
